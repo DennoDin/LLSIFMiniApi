0b032ecaac849b5b780872f4901fee88d89dd07b
Dustin <dustin.tran@live.com>
2018-07-29 20:46:40 +0900

    created basic html homepage and served it using express

--- frontend-services/express.js
+++ frontend-services/express.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const expApp = express();
 expApp.use(express.json());
+const path = require('path');
 
 const OK = 200;
 const FAIL = 404;
 
-const sifExpressServer = () => {
+// const sifExpress = () => {
 
-  //add GET, POST, PUT, DELETE, ETC here
-}
+//add GET, POST, PUT, DELETE, ETC here
+expApp.get("/", (req, res) => {
+  res.status(OK).sendFile('homePage.html', { root: path.join(__dirname, '../') });
+})
+// }
+expApp.get('/idol', (req, res) => {
+  res.status(FAIL).send();
+})
+
+expApp.listen(21192);
